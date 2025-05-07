1752b4bdc00edea22c3fd59afc1dac7caf3d187c
rismamutia <rismamutiadewi89@gmail.com>
2025-05-07 12:49:06 +0700

    proses inputan ulang

--- binaryseacrh.cpp
+++ binaryseacrh.cpp
@@ -68,5 +68,20 @@ void display()
 void binarySearch()
 {
     char ulang;
+    do
+    {
+        cout << "\n===========================================\n";
+        cout << "              Pencarian Binary Search         \n";
+        cout << "==============================================\n";
+        cout << "Masukkan elemen yang ingin dicari: ";
+        cin >> x;
+
+        int low = 0;
+        int high = nPanjang - 1;
+
+        do
+    } while ({
+        
+    });
     
 }
