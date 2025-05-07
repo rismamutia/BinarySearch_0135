09f4ddb83c8ccd1c430aad11c5059c70a54285f6
rismamutia <rismamutiadewi89@gmail.com>
2025-05-07 12:37:43 +0700

    proses void input data menggunakan metode bubble sort

--- binaryseacrh.cpp
+++ binaryseacrh.cpp
@@ -30,3 +30,18 @@ void input()
         cin >> element[i];
     }
 }
+
+void bubblesortArray()
+{
+    int pass = 1;
+    do
+    {
+        for (int j = 0; j <= nPanjang - 1 - pass; j++)
+        {
+            if (element[j] > element[j + 1])
+        }
+    } while ({
+        
+    });
+    
+}
